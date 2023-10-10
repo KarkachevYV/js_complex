b41534a79dbf03470c67f095e4d7d92fdb00cba6
KarkachevYV <Karkach_Y_V@outlook.com>
2023-10-10 19:03:19 +0300

    закончена функциональность плагина модального окна

--- Modal_Window _on_Js/index.js
+++ Modal_Window _on_Js/index.js
@@ -1,1 +1,26 @@
-const modal = $.modal()
+const queen_bees = [
+    {id: 1, title: 'Средне-Русская', price: 1500, img: 'https://th.bing.com/th/id/R.98a747ac8207681d5a3887fe86e86a8a?rik=fuP7%2bwdJHfslyA&riu=http%3a%2f%2fwww.pchelu.com.ua%2fsites%2fdefault%2ffiles%2fpictures%2farticles%2fsrednerusskaya-poroda-pchel.jpg&ehk=V84u6zJls%2fet%2b9wWJouytAvsrfnKYihiMhmMSD1haMg%3d&risl=&pid=ImgRaw&r=0'},
+    {id: 2, title: 'Карпатка', price: 1000, img: 'https://th.bing.com/th/id/R.552fef6dbe8de61a4d7595698e3392de?rik=NTFiiKlkOXAimQ&riu=http%3a%2f%2fwww.pchelu.com.ua%2fsites%2fdefault%2ffiles%2fpictures%2farticles%2fkarpatskaya-poroda-pchel.jpg&ehk=yobkRE7Cb53gQfEjfmsv2O1XGv1pAl1JPGbNOhtdkRA%3d&risl=&pid=ImgRaw&r=0'},
+    {id: 3, title: 'Итальянка', price: 1100, img: 'https://th.bing.com/th/id/R.c17f70f3a29bb7a54a16f13d9bcc17e9?rik=eTnSUQa%2bQttgnw&riu=http%3a%2f%2fwww.pchelu.com.ua%2fsites%2fdefault%2ffiles%2fpictures%2farticles%2fitalyanskaja-poroda-pchel.jpg&ehk=uYJEXya3Opa9AZ%2fYaUpmDd2c8bMUpP0BX%2fY0ZjOxhvQ%3d&risl=&pid=ImgRaw&r=0'}
+]//создадим некий массив для отработки и приминения модального окна на практике... 
+
+
+
+const modal = $.modal({//для дальнейшей реализации модального окна и для реализации плагина, в его options необходимо передать объект
+    title: 'Пасека Каркача', //с неким заголовком у модального окна
+    closable: true, //с некой ключом указывающим на то, что модальное окно закрывается
+    content: `        
+        <p>Lorem ipsum dolor sit.</p>
+    `, //с неким контекстом(содержанием контекста)
+    width: '400px',//с некоторым установленным размером по ширине
+    footerButtons: [
+        {text: 'Ok', type: 'primary', handler() {
+            console.log('Primery btn clicked')
+            modal.close()
+        }},
+        {text: 'Cancel', type: 'danger', handler() {
+            console.log('Denger btn clicked')
+            modal.close()
+        }}
+    ]//и с неким массивом имеющим опреленные свойства, для существующих кнопок у модального окна
+})//эти пары в объекте options автоматически передаются в созданную приватную функцию 
--- Modal_Window _on_Js/plugins/modal.js
+++ Modal_Window _on_Js/plugins/modal.js
@@ -1,51 +1,96 @@
-function _createModal(options) {//создаём некую приватную функцию , о чём говорит нижнее подчёркивание, через замыкания open & close
+Element.prototype.appendAfter = function(element) {//прототип через указанный метод позволяет создать элемент footer в  дом-дереве
+    element.parentNode.insertBefore(this, element.nextSibling)//реализация из стек оверфлоу с необходимой корректировкой
+}
+
+function noop() {}//некая пустая функция
+
+function _createModalFooter(buttons = []) {//создаём некую приватную функцию для кнопок модального окна
+    if (buttons.length === 0) {
+        return document.createElement('div')//условие для создания пустого дива
+    }
+    const wrap = document.createElement('div')//иначе создаётся  корневой элемент нода
+    wrap.classList.add('modal-footer')//и класс у ноды
+//а для создания необходимых кнопок мы используем forEach пробегаясь по которой создаётся необходимое кол-во кнопок
+    buttons.forEach(btn => {//где buttons один из объектов, для которого создаётся нода, со следующими свойствами, они должны быть такие же как в footerButtons
+        const $btn = document.createElement('button')
+        $btn.textContent = btn.text
+        $btn.classList.add('btn')
+        $btn.classList.add(`btn-${btn.type || 'secondary'}`)
+        $btn.onclick = btn.handler || noop//пробежавшись по свойствам и создав их
+
+        wrap.appendChild($btn)//создаём кнопки в нужном месте в модальном 
+    })
+    return wrap//выводим виртуальные кнопки в дом дерево
+}
+
+
+
+function _createModal(options) {//создаём некую приватную функцию , о чём говорит нижнее подчёркивание, через замыкания open & close и передадим в него пары из объекта созданного в плагине, при этом для универсальности будем испоьзовать ${}, в который будем передавать соответствующие пары, при этом для исключения не праваильного поведения сразу же будем устанавливать для пар дефолтные значения, чтобы не появлялись баги.
+    const DEFAULT_WIDTH = '600px'//для ширины модального окна таким дефолтным значениме будет данная переменная, соответственно для всех пар дефолтные значения будут указываться через || в динамическом объекте ${}, кроме булевого значения, для которого будет указан тернарный оператор в случае истины и лжи, при этом, чтобы элемент крестик стал рабочим мы применим к нему дата атрибут, соответственно такой же аргумент мы применим и для поля modal-overlay модального окна со значением true.,
+    // при этом должен быть создан  клик по которому будет обеспечена прослушивание событий, который создаём при помощи addEventListener('click',  listener) 
     const modal = document.createElement('div')// создаём переменную для взаимодействия из модального окна
-    modal.classList.add('ymodal')//при этом обраившись к методу создания классов мы создадим виртуально класс vmodal
+    modal.classList.add('ymodal')//при этом обраившись к методу создания классов мы создадим виртуально класс ymodal
     modal.insertAdjacentHTML('afterbegin', `        
-        <div class="modal-overlay">
-            <div class="modal-window">
+        <div class="modal-overlay" data-close="true">
+            <div class="modal-window" style="width: ${options.width || DEFAULT_WIDTH}">
                 <div class="modal-header">
-                    <span class="modal-title">Modal title</span>
-                    <span class="modal-close">&times;</span>
+                    <span class="modal-title">${options.title || 'Окно'}</span>
+                    ${options.closable ? `<span class="modal-close" data-close="true">&times;</span>` : ''}
                 </div>
-                <div class="modal-body">
-                    <p>Lorem ipsum dolor sit.</p>
-                    <p>Lorem ipsum dolor sit.</p>
-                </div>
-                <div class="modal-footer">
-                <button type="button">Ok</button>
-                <button type="button">Cancel</button>
+                <div class="modal-body" data-content>
+                    ${options.content || ''}                   
                 </div> 
             </div> 
         </div>        
-    `)//а обратившиь к метоу создания структур html  мы вставим вторым параметром вырезанный рабочий кусок из кода body , тем самым виртуально создалим модальное окно, будучи уже созданное в коде html, при этом первый div  с классом vmodal не включаем, ибо он виртуально создан выше по коду, вставляем в некую позицию :'afterbegin', которая указывается первым параметром, в данном методе.
-    document.body.appendChild(modal)//предварительно вставив modal в дом-дерево, с помощью метода appendChild
+    `)//а обратившиь к методу создания структур html  мы вставим вторым параметром вырезанный рабочий кусок из кода body , тем самым виртуально создалим модальное окно, будучи уже созданное в коде html, при этом первый div  с классом ymodal не включаем, ибо он виртуально создан выше по коду, вставляем в некую позицию :'afterbegin', которая указывается первым параметром, в данном методе.
+    const footer = _createModalFooter(options.footerButtons)//создаём ноду футтера
+    footer.appendAfter(modal.querySelector('[data-content]'))//вставляем ноду футтера в нужное место ноды модального окна
+    document.body.appendChild(modal)// вставляем modal в дом-дерево, с помощью метода appendChild
     return modal//и возвращаем виртуальную переменную
 }
 
 
-$.modal = function(options) {//создаём свой плагин
+$.modal = function(options) {//создаём свой плагин(node)
     // после создания приватной функции  и виртуального модального окна в виде modal, мы её вызываем в своём плагине, для чего создаём приватную переменную через $.
-    const  ANIMATION_SPEED = 200    
-    const $modal = _createModal(options)// $ - говорит о том, что виртуальная переменная создана при помощи метода создания тегов
-    let closing = false
+    const  ANIMATION_SPEED = 200    //данная переменная создаётсядля анимации модального окна
+    const $modal = _createModal(options)// $ - говорит о том, что виртуальная переменная(элемент)создана(н) при помощи метода создания тегов
+    let closing = false//вводятся 1 изменяемая переменная(флаг) указывающая на факт закрытия модального окна
+    let destroyed = false//вводится 2 изменяемая переменная(флаг) указывающая на факт ликвидации модального окна и его слушателя, которая является элементом защиты кода, которые обеспечивают условия работы всех методов модального окна
     
-    
-    
-        return {//с использованием замыкания
-            
-            open() {
-                !closing && $modal.classList.add('open')
-            },
-            close() {
-                closing = true
-                $modal.classList.remove('open')
-                $modal.classList.add('hide')
-                setTimeout(() => {
-                    $modal.classList.remove('hide')
-                    closing = false
-                }, ANIMATION_SPEED)
-            },
-            destroy() {}
+    const modal = {
+        open() {
+            if (destroyed) {//создав данную проверку 
+                return console.log('Modal is destroyed')
+            }
+            !closing && $modal.classList.add('open')
+        },
+        close() {
+            closing = true
+            $modal.classList.remove('open')
+            $modal.classList.add('hide')
+            setTimeout(() => {
+                $modal.classList.remove('hide')
+                closing = false
+            }, ANIMATION_SPEED)
         }
     }
+
+    const listener = event => {//функция создана для легкого удаления слушателя 
+        if (event.target.dataset.close) {
+            modal.close()//вызов метода закрывает модальное окно
+        }
+    }
+
+    $modal.addEventListener('click',  listener)//после создания клика,  клик(слушатель) должен быть удален, и для этого используется метод destroy(разрушитель)
+    
+    return Object.assign(modal, {//который мы создаём в публичном объекте через вызов метода assign из глобального объекта Object, для которого первым парметром является переменная объект включающая все описанные методы модального окна, а вторым метод  destroy который добавляется(объявляется) в переменную modal
+        destroy() {//который имеет необходимый функционал:
+            $modal.parentNode.removeChild($modal)//обычный способ удаления модального окна (node) из дом дерева
+            $modal.removeEventListener('click', listener)//обычный способ удаления слушателя(клика)
+            destroyed = true//при этом переменная проверки фиксирует данные удаления,  значением истины
+        },
+        setContent(html) {// и второй дополнительный метод, позволяющий динамически менять любое содержимое модального окна
+            $modal.querySelector('[data-content]').innerHTML =html
+        }// метод создающий контекст модального окна через setContent
+    })
+}
