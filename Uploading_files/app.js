c12a06eee309bce4cf656103a2e1d4b875e4245f
KarkachevYV <Karkach_Y_V@outlook.com>
2023-10-22 13:23:56 +0300

    организация загрузки файлов на CSS и  JS

---
+++ Uploading_files/app.js
@@ -0,0 +1,12 @@
+// import { upload } from "upload.js";
+
+
+upload('#file', {
+    multi: true,
+    accept: ['.png', '.jpg', '.jpeg', '.gif'],
+    onUpload(files) {
+        console.log('Files', files)
+    }
+})
+
+// console.log('app.js')
